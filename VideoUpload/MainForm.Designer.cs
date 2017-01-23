7469ce59a9f7fcb258ab91e493fa19b8e60473cb
lijiangbo <245730400@qq.com>
2017-01-23 23:52:49 +0800

    编写界面

--- VideoUpload/MainForm.Designer.cs
+++ VideoUpload/MainForm.Designer.cs
@@ -31,45 +31,58 @@ namespace VideoUpload
         {
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(8, 339);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(90, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "选择视频";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(511, 12);
+            this.progressBar1.Location = new System.Drawing.Point(468, 339);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(289, 23);
             this.progressBar1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始上传";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(8, 3);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(788, 297);
-            this.listView1.TabIndex = 2;
+            this.listView1.Size = new System.Drawing.Size(793, 330);
+            this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Tile;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 350);
+            this.ClientSize = new System.Drawing.Size(813, 379);
             this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Name = "MainForm";
             this.Text = "视频上传";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
@@ -78,7 +91,8 @@ namespace VideoUpload
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ListView listView1;
+        private Button button2;
+        private ListView listView1;
     }
 }
 
--- VideoUpload/MainForm.cs
+++ VideoUpload/MainForm.cs
@@ -15,25 +15,67 @@ namespace VideoUpload
         public MainForm()
         {
             InitializeComponent();
+            listView1.View = View.Details;
         }
 
+        //选择视频按钮事件
         private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();     //显示选择文件对话框
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ShowDialog();
+        }
+
+        //开始上传按钮事件
+        private void button2_Click(object sender, EventArgs e)
         {
             //this.progressBar1.Minimum = 0;
             //this.progressBar1.Maximum = 100;
             //this.progressBar1.BackColor = Color.Red;
 
-            //for (int i = 0; i < 100;i++ )
+            //for (int i = 0; i < 100; i++)
             //{
             //    this.progressBar1.Value++;
             //    Application.DoEvents();
             //}
 
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.FileName = "openFileDialog1";
-            ofd.Multiselect = true;
+            
+
+            listView1.BeginUpdate();
+
+            
+
+            
+
+            for (int i = 0; i < 10;i++ )
+            {
+                listView1.Items.Add("row1"+i, i+"", i);
+                listView1.Items["row1" + i].SubItems.Add("200KB" + i);
+                listView1.Items["row1" + i].SubItems.Add("test.mp4" + i);
+                
+
+               
+
+                listView1.Items["row1" + i].SubItems.Add("4" + i);
+                listView1.Items["row1" + i].BackColor = Color.Green;
+
+                listView1.Items["row1" + i].SubItems.Add("等待上传" + i);
+            }
+
+            listView1.EndUpdate();
         }
 
-        
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //初始化表头
+            listView1.Columns.Add("编号", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("大小", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("名称", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("进度", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("状态", 100, HorizontalAlignment.Center);
+        }
     }
 }
